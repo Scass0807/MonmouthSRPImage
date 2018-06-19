ac816873bdf60eccf96e29f42d7b15626b63abb1
Mahmoud Shabana <mshabana5656@gmail.com>
2018-06-19 13:39:02 -0400

    implemented data storage for feature matching points

--- src/FeatureMatching.py
+++ src/FeatureMatching.py
@@ -15,7 +15,31 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
 matches = bf.match(des1, des2)
 matches = sorted(matches, key = lambda x:x.distance)
 
+# Initialize lists
+list_kp1 = []
+list_kp2 = []
+
+# For each match...
+for mat in matches:
+    
+    # Get the matching keypoints for each of the images
+    img1_idx = mat.queryIdx
+    img2_idx = mat.trainIdx
+    
+    # x - columns
+    # y - rows
+    # Get the coordinates
+    (x1,y1) = kp1[img1_idx].pt
+    (x2,y2) = kp2[img2_idx].pt
+    
+    # Append to each list
+    list_kp1.append((x1, y1))
+    list_kp2.append((x2, y2))
+
+print (list_kp1, list_kp2)
+
+'''
 img3 = cv2.drawMatches(img1,kp1,img2,kp2, matches[:20], None, flags=2)
 plt.imshow(img3)
 plt.show()
-
+'''
