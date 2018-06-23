3f855b380b69b7976c1a5c3101337483e8dcaf38
Mahmoud Shabana <mshabana5656@gmail.com>
2018-06-23 14:21:50 -0400

    edits

--- src/fileproperties.py
+++ src/fileproperties.py
@@ -8,13 +8,29 @@ from exifread import process_file, exif_log, __version__
 from os import listdir
 from os.path import isfile, join
 
-#Specify where pictures are found
-subjectdir='C:/Users/geckert/Downloads/CellTower/'
+#must install re first!
+import re
 
-onlyfiles = [f for f in listdir(subjectdir) if isfile(join(subjectdir, f))]
+def sorted_nicely( l ):
+    """ Sorts the given iterable in the way that is expected.
+ 
+    Required arguments:
+    l -- The iterable to be sorted.
+ 
+    """
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    return sorted(l, key = alphanum_key)
+
+#Specify where pictures are found
+subjectdir='/Users/Steven/Desktop/Projects/SRP/MonmouthSRPImage/Images/raw_images/'
+
+onlyfiles = [f for f in listdir(subjectdir) if isfile(join(subjectdir, f)) and f[len(f)-4:]=='.JPG']
+
+onlyfiles = sorted_nicely(onlyfiles)
 
 #open output file and write headings -extract 'GPS' headings only
-outfile=open("C:/Users/geckert/Downloads/datafile.csv", 'w')
+outfile=open("/Users/Steven/Desktop/Projects/SRP/MonmouthSRPImage/src/project_files/datafile.csv", 'w')
 headings="FileName, GPS GPSAltitude Ratio, GPS GPSAltitudeRef Byte, GPS GPSLatitude Ratio, GPS GPSLatitudeRef ASCII, GPS GPSLongitude Ratio, GPS GPSLongitudeRef ASCII, GPS GPSVersionID Byte\n"
 outfile.write(headings)
 
@@ -33,11 +49,14 @@ for fname in onlyfiles:
         
     tag_keys = list(data.keys())
     tag_keys.sort()
+    print(tag_keys)
+    
     for i in tag_keys:
 
         #output GPS tags only
         if i[0:3]=='GPS':
             try:
+                print(i)
                 filestuff = filestuff+(data[i].printable).replace(',',':')+","
             except:
                 print(i, str(data[i]))
