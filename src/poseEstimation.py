ab161cadee474e3f1b5052109e88f30e387c6fcf
Mahmoud Shabana <mshabana5656@gmail.com>
2018-07-16 14:34:10 -0400

    changed file name

--- src/poseEstimation.py
+++ src/poseEstimation.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob
 
 # Load previously saved data
-with np.load('B.npz') as X:
+with np.load('webcam_calibration_output_2.npz') as X:
     mtx, dist, _, _ = [X[i] for i in ('mtx','dist','rvecs','tvecs')]
 
 def draw(img, corners, imgpts):
