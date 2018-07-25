5b8ebd9699a0878ecd82725c40fe29508a6fca7d
Tian1996 <40369357+Tian1996@users.noreply.github.com>
2018-07-25 15:14:50 -0400

    finally

--- src/CameraPosition.py
+++ src/CameraPosition.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 import csv
 from Point2d import Point2d
+import GetMatrix
 points1 = []
 points2 = []
-with open('C:/Users/nianq/OneDrive/Documents/GitHub/MonmouthSRPImage/src/project_files/24.JPG_14.JPG_matches.csv', 'rb') as csvfile:
+with open('C:/Users/nianq/OneDrive/Documents/GitHub/MonmouthSRPImage/src/project_files/14.JPG_29.JPG_matches.csv', 'rb') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
      for row in spamreader:
-         points1.append(row[0:1])
-         points2.append(row[1:2])
+         points1.append(row[2:4])
+         points2.append(row[0:2])
 
 points2.pop(0)
 points1.pop(0)
@@ -16,28 +17,36 @@ points1.pop(0)
 # Example. Estimation of fundamental matrix using the RANSAC algorithm
 
 #find the camera matrix
-import GetMatrix
+
 mtx = GetMatrix.cameraMatrix
-print(mtx[0][2])
+mtx = np.array(mtx,dtype = np.float32)
+
 points1 = np.array(points1,dtype = np.float32)
 points2 = np.array(points2,dtype = np.float32)
 
-pictureWL = Point2d(2000,3000)
+pictureWL = (3000,4000)
 
-print type(points1[0][0])
+
 #find the essential matrix using the results of the fundamental matrix
 # K must be found before hand
-essential_matrix = cv2.findEssentialMat(points1, points2, 3.61, pictureWL,8, 0.99, 1.0)
-print essential_matrix
+essential_matrix, mask = cv2.findEssentialMat(points1, points2, 3.61, pictureWL,8, 0.99, 1.0)
+
+#Derive R and Tx from the u, s, and v
+
+rotation = cv2.recoverPose(essential_matrix, points1, points2, mtx,3.61,mask)
+
+R = cv2.Rodrigues(rotation[1])
+translation = cv2.Rodrigues(rotation[2])
+
+R = np.array(R[0]).transpose()
+t = np.matmul(-1*R, translation[0])
+print t
 
 '''
 fundamental_matrix = findFundamentalMat(points1, points2, FM_RANSAC, 3, 0.99)
 #Compute the Singular Value Decomposition of E
 #u, s, vh = np.linalg.svd(essential_matrix, full_matrices=True)
 
-
-#Derive R and Tx from the u, s, and vh
-recoverPose(E, points1, points2, mtx, rotation, translation, mask)
 
 #Retrieve camera position 
 Rodrigues(rotation, R)
