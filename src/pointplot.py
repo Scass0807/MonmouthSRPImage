07c8ac265d83e4bc74e6a8c96f647298fe97ab69
Steven Cassidy <scass0807@gmail.com>
2018-08-08 11:43:18 -0400

    added scatter

--- src/pointplot.py
+++ src/pointplot.py
@@ -1,15 +1,38 @@
 from matplotlib import pyplot
 from mpl_toolkits.mplot3d import Axes3D
-import random
+import numpy as np
 import Height
-
 fig = pyplot.figure()
 ax = Axes3D(fig)
-
+imagePath = Height.subjectdir
 points = Height.GPS
 print (points)
-for i in range(0,len(points)-1):
-    ax.scatter(points[i][1],points[i][2],points[i][3])
+ax = pyplot.subplot2grid((4,9),(0,0),rowspan = 4,colspan=5,projection='3d')
+for i in range(0,len(points)):
+    ax.scatter(points[i][1],points[i][2],points[i][3], color='orange',picker=True)
+def FindImage(x,y,z):
+    for i in range(len(points)):
+        if(x==points[i][1] and y == points[i][2] and z == points[i][3]):
+            return points[i][0]
+    return 'Not found'
 
-
+ax2 = pyplot.subplot2grid((4,9),(0,5),rowspan =4,colspan=4)
+highlight = ax.scatter([], [],[],color='blue')               
+def onpick(event):
+    global imagePath
+    global highlight
+    highlight.remove()
+    ind = event.ind[0]
+    x, y, z = event.artist._offsets3d
+    print((x[ind],y[ind],z[ind]))
+    highlight = ax.scatter(x[ind],y[ind],z[ind], color='blue')
+    selectedImageName = FindImage(x[ind],y[ind],z[ind])
+    if selectedImageName != 'Not found':
+        selectedImageName = imagePath + selectedImageName
+        selectedImage = pyplot.imread(selectedImageName)
+        ax2.imshow(selectedImage)
+        
+        ax2.imshow(selectedImage)
+    pyplot.draw()
+fig.canvas.mpl_connect('pick_event', onpick)
 pyplot.show()
